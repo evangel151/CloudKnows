e932daf4d1bbb55e1eb7ba2d249804acbe1515c4
liji <154906186@qq.com>
2016-07-29 00:02:25 +0800

    Initial Commit

---
+++ CloudKnows/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CloudKnows
+//
+//  Created by  a on 16/7/29.
+//  Copyright © 2016年 ycdsq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CloudKnows/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CloudKnows
+//
+//  Created by  a on 16/7/29.
+//  Copyright © 2016年 ycdsq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
